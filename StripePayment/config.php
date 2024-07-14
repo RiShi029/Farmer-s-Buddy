1f88fa25078927f873d6092dfcd2948e5943d0fb
Rishi Shivhare <68809661+RiShi029@users.noreply.github.com>
2024-07-14 11:07:04 +0530

    Update config.php

--- StripePayment/config.php
+++ StripePayment/config.php
@@ -3,8 +3,8 @@
 	require_once "products.php";
 
 	$stripeDetails = array(
-		"secretKey" => "sk_test_51OwNfvSFXdCF6QlR7Kf2NMmTM2j9TAqGALjrRK1kSmD0f3mxGJwQUlOktJSppKmQIsyCf8d7slLcviplkyefCMYd00M4bduZIe",  // Stripe Secret key
-		"publishableKey" => "pk_test_51OwNfvSFXdCF6QlRUqRWyzVR89XivgHZyKcRxOBH9rcJSH6GRHaMbTf1O66p7KsBIZkSzZTaVix5PiZJS82s5F8000SEWtKPML"  //Stripe Publishable key
+		"secretKey" => "",  // Stripe Secret key
+		"publishableKey" => ""  //Stripe Publishable key
 	);
 
 	// Set secret key: remember to change this to live secret key in production
